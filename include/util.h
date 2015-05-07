25f735b93aaed3dc70e4bbe101164af423102f27
moon87.park <moon87.park@samsung.com>
2015-05-07 14:58:05 +0900

    Merge tizen2.4 starter
    Change-Id: Ia9dec0b534b8d89f8bb303bf6107089b0f56c2cf

--- include/util.h
+++ include/util.h
@@ -1,33 +1,48 @@
- /*
-  * Copyright 2012  Samsung Electronics Co., Ltd
-  *
-  * Licensed under the Flora License, Version 1.1 (the "License");
-  * you may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at
-  *
-  *     http://floralicense.org/license/
-  *
-  * Unless required by applicable law or agreed to in writing, software
-  * distributed under the License is distributed on an "AS IS" BASIS,
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  */
-
-
+/*
+ * Copyright (c) 2000 - 2015 Samsung Electronics Co., Ltd. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #ifndef __MENU_DAEMON_UTIL_H__
 #define __MENU_DAEMON_UTIL_H__
 #include <dlog.h>
 #include <stdio.h>
 #include <sys/time.h>
+#include <app.h>
 
-#define HOME_SCREEN_PKG_NAME "org.tizen.menu-screen"
+#define APP_TRAY_PKG_NAME "org.tizen.app-tray"
+#define MENU_SCREEN_PKG_NAME "org.tizen.menu-screen"
+#define PROVIDER_PKG_NAME "org.tizen.data-provider-master"
+#define CLUSTER_HOME_PKG_NAME "org.tizen.cluster-home"
+#define EASY_HOME_PKG_NAME "org.tizen.easy-home"
+#define EASY_APPS_PKG_NAME "org.tizen.easy-apps"
+#define HOMESCREEN_PKG_NAME "org.tizen.homescreen"
+#define TASKMGR_PKG_NAME "org.tizen.task-mgr"
+#define DEFAULT_TASKMGR_PKG_NAME "org.tizen.taskmgr"
 #define CONF_PATH_NUMBER 1024
+
+#define BUF_SIZE_16 16
+#define BUF_SIZE_32 32
+#define BUF_SIZE_128 128
+#define BUF_SIZE_256 256
+#define BUF_SIZE_512 512
+#define BUF_SIZE_1024 1024
+
 
 #ifdef  LOG_TAG
 #undef  LOG_TAG
-#define LOG_TAG "starter"
+#define LOG_TAG "STARTER"
 #endif
 
 /* Log */
@@ -41,6 +56,18 @@
 
 #if !defined(_E)
 #define _E(fmt, arg...) LOGE("[%s:%d] "fmt"\n", __func__, __LINE__, ##arg)
+#endif
+
+#if !defined(_SECURE_W)
+#define _SECURE_W(fmt, arg...) SECURE_LOGW("[%s:%d] "fmt"\n", __func__, __LINE__, ##arg)
+#endif
+
+#if !defined(_SECURE_D)
+#define _SECURE_D(fmt, arg...) SECURE_LOGD("[%s:%d] "fmt"\n", __func__, __LINE__, ##arg)
+#endif
+
+#if !defined(_SECURE_E)
+#define _SECURE_E(fmt, arg...) SECURE_LOGE("[%s:%d] "fmt"\n", __func__, __LINE__, ##arg)
 #endif
 
 #define retv_if(expr, val) do { \
@@ -92,6 +119,13 @@
 	fclose(fp);\
 } while (0)
 
+#if !defined(_)
+#define _(str) dgettext(PACKAGE, str)
+#endif
+
+#if !defined(S_)
+#define S_(str) dgettext("sys_string", str)
+#endif
 
 
 #endif /* __MENU_DAEMON_UTIL_H__ */
