a9b959f33d8c337682a0789b75d7eab38d97498c
gs86.lee <gs86.lee@samsung.com>
2015-09-10 20:40:55 +0900

    Implement keygrab by ecore API : ecore_wl_window_keygrab_set()
    Change-Id: I60096c236a3011424c3493562cd8c41987d1eafd

--- include/mobile/hw_key.h
+++ include/mobile/hw_key.h
@@ -17,25 +17,68 @@
 #ifndef __HW_KEY_H__
 #define __HW_KEY_H__
 
-#ifdef HVAE_X11
-#define	KEY_VOLUMEUP	"XF86AudioRaiseVolume"
-#define	KEY_VOLUMEDOWN	"XF86AudioLowerVolume"
-#define	KEY_HOME	"XF86Home"
-#define	KEY_CONFIG	"XF86Camera_Full"
-#define	KEY_SEARCH	"XF86Search"
-#define	KEY_MEDIA	"XF86AudioMedia"
-#define	KEY_TASKSWITCH	"XF86TaskPane"
-#define	KEY_WEBPAGE	"XF86WWW"
-#define	KEY_MAIL	"XF86Mail"
-#define	KEY_VOICE	"XF86Voice"
-#define	KEY_APPS	"XF86Apps"
-#define	KEY_CONNECT	"XF86Call"
-#define KEY_BACK	"XF86Back"
+#if HAVE_X11
+
+#define KEY_VOLUMEUP    "XF86AudioRaiseVolume"
+#define KEY_VOLUMEDOWN  "XF86AudioLowerVolume"
+#define KEY_HOME        "XF86Home"
+#define KEY_CONFIG      "XF86Camera_Full"
+#define KEY_SEARCH      "XF86Search"
+#define KEY_MEDIA       "XF86AudioMedia"
+#define KEY_TASKSWITCH  "XF86TaskPane"
+#define KEY_WEBPAGE     "XF86WWW"
+#define KEY_MAIL        "XF86Mail"
+#define KEY_VOICE       "XF86Voice"
+#define KEY_APPS        "XF86Apps"
+#define KEY_CONNECT     "XF86Call"
+#define KEY_BACK       "XF86Back"
+
+#elif HAVE_WAYLAND
+
+typedef enum {
+	KEY_VOLUMEUP = 0,
+	KEY_VOLUMEDOWN = 1,
+	KEY_POWER = 2,
+	KEY_MENU,
+	KEY_HOME,
+	KEY_BACK,
+	KLEY_CAMERA,
+	KEY_CONFIG,
+	KEY_SEARCH,
+	KEY_PLAYCD,
+	KEY_PAUSECB,
+	KEY_STOPCD,
+	KEY_NEXTSONG,
+	KEY_PRIVEIOUSSONG,
+	KEY_REWIND,
+	KEY_FASTFORWARD,
+	KEY_MEDIA,
+	KEY_PLAYPAUSE,
+	KEY_MUTE,
+	KEY_REC,
+	KEY_CANCEL,
+	KEY_SOFTBD,
+	KEY_QUICKPANEL,
+	KEY_TASKSWITCH,
+	KEY_HOMEPAGE,
+	KEY_WEBPAGE,
+	KEY_MAIL,
+	KEY_SCREENSAVER,
+	KEY_BRIGHTNESSDOWN,
+	KEY_BRIGHTNESSUP,
+	KEY_VOICE,
+	KEY_LANGUAGE,
+	KEY_APPS,
+	KEY_CONNECT,
+	KEY_GAMEPLAY,
+	KEY_VOICEWAKEUP_LPSD,
+	KEY_VOICEWAKEUP,
+	KEY_NAME_MAX,
+} key_name_e;
+#endif
 
 extern void hw_key_destroy_window(void);
 extern void hw_key_create_window(void);
-#endif //HAVE_X11
 
 #endif
-
 // End of a file
